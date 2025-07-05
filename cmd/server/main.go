b4399c143b09ae5a78a0e89fefc623354fd2118e
Guilherme Gatti Marinho <ggattimarinho@gmail.com>
2025-07-05 04:07:37 -0300

    feat: Initialize database connection in main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/guilherme-gatti/poc_scorm/internal/router"
+	"github.com/guilherme-gatti/poc_scorm/internal/storage"
 )
 
 func main() {
+	storage.InitDB("storage/database.db")
+
 	r := router.SetupRouter()
 	r.Run(":3000")
 }
